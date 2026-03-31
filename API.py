e649f499f1c280440578f49f3828f5d53ad0f7f5
gustavomendes67 <guga1214outlook@gmail.com>
2026-03-31 18:32:10 -0400

    Entrada de nomes de cidades e busca de coordenadas/climas

--- API.py
+++ API.py
@@ -1,4 +1,29 @@
 import requests
 
-asd
- #alteração
+API_KEY = '8e882ea9fbb3b97bbe17f1c764f79674'
+
+def obter_clima(cidade):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&units=metric&lang=pt_br'
+
+    resposta = requests.get(url)
+    dados = resposta.json()
+
+    return {
+        'cidade': cidade,
+        'temperatura': dados['main']['temp'],
+        'clima': dados['weather'] [0] ['description']
+    }
+
+def obter_cidades():
+    cidades = input('Digite 5 cidades separadas por vírgula: ').strip().split(',')
+
+    dados_cidades = []
+
+    for cidade in cidades:
+        try:
+            dados = obter_clima(cidade.strip())
+            dados_cidades.append(dados)
+        except:
+            print(f'Erro ao buscar cidade: {cidade}')
+
+    return dados_cidades
